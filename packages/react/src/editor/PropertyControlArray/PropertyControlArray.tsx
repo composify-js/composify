32239c544431449c46223113104ae095627a91a8
Injung Chung <injung.chung@gmail.com>
2025-07-26 23:42:49 +0900

    fix: place remove button at the bottom for object item types

--- packages/react/src/editor/PropertyControlArray/PropertyControlArray.tsx
+++ packages/react/src/editor/PropertyControlArray/PropertyControlArray.tsx
@@ -43,7 +43,7 @@ export const PropertyControlArray = ({ name, spec, value, ...props }: Props) => 
         return (
           <div className={getClassName()}>
             {values.map((value, index) => (
-              <div key={`${name}-${index}`} className={getClassName('Item')}>
+              <div key={`${name}-${index}`} className={getClassName('Item', { object: spec.item.type === 'object' })}>
                 <div className={getClassName('ItemContent')}>
                   {(() => {
                     switch (spec.item.type) {
@@ -113,7 +113,10 @@ export const PropertyControlArray = ({ name, spec, value, ...props }: Props) => 
                     }
                   })()}
                 </div>
-                <button className={getClassName('RemoveButton')} onClick={handleClickRemove(index)}>
+                <button
+                  className={getClassName('RemoveButton', { object: spec.item.type === 'object' })}
+                  onClick={handleClickRemove(index)}
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 640 640">
                     <path d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z" />
                   </svg>
