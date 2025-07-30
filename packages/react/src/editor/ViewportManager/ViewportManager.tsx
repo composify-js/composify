23d2370a10c88ae94683768b9698299735b23dce
Injung Chung <injung.chung@gmail.com>
2025-07-30 21:22:30 +0900

    feat: add ViewportScaler component

--- packages/react/src/editor/ViewportManager/ViewportManager.tsx
+++ packages/react/src/editor/ViewportManager/ViewportManager.tsx
@@ -1,7 +1,7 @@
 import { getClassNameFactory } from '@composify/utils';
-import { getBox } from 'css-box-model';
-import { FC, PropsWithChildren, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, PropsWithChildren, useState } from 'react';
 import { ViewportControl, ViewportControlProps } from '../ViewportControl';
+import { ViewportScaler } from '../ViewportScaler';
 import styles from './ViewportManager.module.css';
 
 type Props = PropsWithChildren<Pick<ViewportControlProps, 'viewports'>>;
@@ -9,43 +9,13 @@ type Props = PropsWithChildren<Pick<ViewportControlProps, 'viewports'>>;
 const getClassName = getClassNameFactory('ViewportManager', styles);
 
 export const ViewportManager: FC<Props> = ({ children }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  const [scale, setScale] = useState(1);
   const [width, setWidth] = useState(375);
 
-  const autoScale = useCallback(() => {
-    if (!containerRef.current) {
-      return;
-    }
-
-    const container = getBox(containerRef.current);
-    const scale = Math.min(container.contentBox.width / width, 2);
-
-    setScale(scale);
-  }, [width]);
-
-  useEffect(autoScale, [autoScale]);
-
-  useEffect(() => {
-    window.addEventListener('resize', autoScale);
-
-    return () => {
-      window.removeEventListener('resize', autoScale);
-    };
-  }, [autoScale]);
-
   return (
-    <section ref={containerRef} className={getClassName('Container')}>
+    <section className={getClassName()}>
       <ViewportControl onClick={setWidth} />
-      <div
-        className={getClassName('Target')}
-        style={{
-          width,
-          transform: `scale(${scale})`,
-        }}
-      >
-        {children}
+      <div className={getClassName('Content')}>
+        <ViewportScaler width={width}>{children}</ViewportScaler>
       </div>
     </section>
   );
---
+++ packages/react/src/editor/ViewportScaler/ViewportScaler.tsx
@@ -0,0 +1,51 @@
+import { getClassNameFactory } from '@composify/utils';
+import { getBox } from 'css-box-model';
+import { FC, PropsWithChildren, useCallback, useEffect, useRef, useState } from 'react';
+import styles from './ViewportScaler.module.css';
+
+const getClassName = getClassNameFactory('ViewportScaler', styles);
+
+type Props = PropsWithChildren<{
+  width: number;
+}>;
+
+export const ViewportScaler: FC<Props> = ({ width, children }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [scale, setScale] = useState(1);
+
+  const autoScale = useCallback(() => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const container = getBox(containerRef.current);
+    const scale = Math.min(container.contentBox.width / width, 2);
+
+    setScale(scale);
+  }, [width]);
+
+  useEffect(() => {
+    requestAnimationFrame(autoScale);
+  }, [autoScale]);
+
+  useEffect(() => {
+    window.addEventListener('resize', autoScale);
+
+    return () => window.removeEventListener('resize', autoScale);
+  }, [autoScale]);
+
+  return (
+    <>
+      <div ref={containerRef} className={getClassName('Container')} />
+      <div
+        className={getClassName('Target')}
+        style={{
+          width,
+          transform: `scale(${scale})`,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
