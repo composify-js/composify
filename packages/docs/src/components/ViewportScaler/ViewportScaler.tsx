df8fd9353a528a7ed2bfdc53b5dcf4025afb01e5
Injung Chung <injung.chung@gmail.com>
2025-08-05 21:23:15 +0900

    feat: scale Playground according to page size

---
+++ packages/docs/src/components/ViewportScaler/ViewportScaler.tsx
@@ -0,0 +1,54 @@
+import { getBox } from 'css-box-model';
+import { FC, PropsWithChildren, useCallback, useEffect, useRef, useState } from 'react';
+import styles from './ViewportScaler.module.css';
+
+type Props = PropsWithChildren<{
+  width: number;
+}>;
+
+export const ViewportScaler: FC<Props> = ({ width, children }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [scale, setScale] = useState(1);
+
+  const autoScale = useCallback(() => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const box = getBox(containerRef.current);
+    const scale = Math.min(box.contentBox.width / width, 1);
+
+    setScale(scale);
+  }, [width]);
+
+  useEffect(() => {
+    requestAnimationFrame(autoScale);
+  }, [autoScale]);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(autoScale);
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [autoScale]);
+
+  return (
+    <>
+      <div ref={containerRef} className={styles.container} />
+      <div
+        className={styles.target}
+        style={{
+          width,
+          transform: `scale(${scale})`,
+        }}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
--- packages/react/src/editor/IsolatedDndProvider/IsolatedDndProvider.tsx
+++ packages/react/src/editor/IsolatedDndProvider/IsolatedDndProvider.tsx
@@ -1,6 +1,10 @@
+import { getClassNameFactory } from '@composify/utils';
 import { FC, PropsWithChildren, useMemo, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import styles from './IsolatedDndProvider.module.css';
+
+const getClassName = getClassNameFactory('IsolatedDndProvider', styles);
 
 export const IsolatedDndProvider: FC<PropsWithChildren> = ({ children }) => {
   const [wrapper, setWrapper] = useState<HTMLDivElement | null>(null);
@@ -8,7 +12,7 @@ export const IsolatedDndProvider: FC<PropsWithChildren> = ({ children }) => {
   const backendOptions = useMemo(() => ({ rootElement: wrapper }), [wrapper]);
 
   return (
-    <div ref={setWrapper}>
+    <div ref={setWrapper} className={getClassName()}>
       {wrapper && (
         <DndProvider backend={HTML5Backend} options={backendOptions}>
           {children}
