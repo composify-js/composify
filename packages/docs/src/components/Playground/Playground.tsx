2c3201538f9eddd1ba6b8d6eb18e490245eb2557
Injung Chung <injung.chung@gmail.com>
2025-08-05 22:09:46 +0900

    fix: show captions under preview

--- packages/docs/src/components/Playground/Playground.tsx
+++ packages/docs/src/components/Playground/Playground.tsx
@@ -19,42 +19,47 @@ const source = `
 
 export const Playground = () => (
   <section className={styles.playground}>
-    <ViewportScaler width={PLAYGROUND_WIDTH}>
-      <Editor
-        title="Home Page"
-        source={source}
-        viewports={[
-          {
-            width: 320,
-            label: 'Mobile S - 320px',
-          },
-          {
-            width: 375,
-            label: 'Mobile M - 375px',
-          },
-          {
-            width: 425,
-            label: 'Mobile L - 425px',
-          },
-          {
-            width: 768,
-            label: 'Tablet - 768px',
-            initial: true,
-          },
-          {
-            width: 1024,
-            label: 'Laptop - 1024px',
-          },
-          {
-            width: 1440,
-            label: 'Desktop - 1440px',
-          },
-          {
-            width: 2560,
-            label: '4k - 1440px',
-          },
-        ]}
-      />
-    </ViewportScaler>
+    <div className={styles.preview}>
+      <ViewportScaler width={PLAYGROUND_WIDTH}>
+        <Editor
+          title="Home Page"
+          source={source}
+          viewports={[
+            {
+              width: 320,
+              label: 'Mobile S - 320px',
+            },
+            {
+              width: 375,
+              label: 'Mobile M - 375px',
+            },
+            {
+              width: 425,
+              label: 'Mobile L - 425px',
+            },
+            {
+              width: 768,
+              label: 'Tablet - 768px',
+              initial: true,
+            },
+            {
+              width: 1024,
+              label: 'Laptop - 1024px',
+            },
+            {
+              width: 1440,
+              label: 'Desktop - 1440px',
+            },
+            {
+              width: 2560,
+              label: '4k - 1440px',
+            },
+          ]}
+        />
+      </ViewportScaler>
+    </div>
+    <p className={styles.description}>
+      Try our editor — the rendered result appears <a href="/preview">here</a>.
+    </p>
   </section>
 );
