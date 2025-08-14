d0ad9f7b7e5f665c9fab15cdf696373a89bba1e1
Injung Chung <injung.chung@gmail.com>
2025-08-14 22:38:05 +0900

    feat: add hero cta

--- packages/docs/src/CloudHeroBanner/CloudHeroBanner.tsx
+++ packages/docs/src/CloudHeroBanner/CloudHeroBanner.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { type FC } from 'react';
 import styles from './CloudHeroBanner.module.css';
 
@@ -5,5 +6,13 @@ export const CloudHeroBanner: FC = () => (
   <section className={styles.heroBanner}>
     <h1 className={styles.title}>Supercharge your editor</h1>
     <p className={styles.description}>All the power you need, without the hassle.</p>
+    <div className={styles.buttonGroup}>
+      <a href="https://app.composify.net" className={clsx(styles.button, styles.learnMore)}>
+        Start free ›
+      </a>
+      <a href="#features" className={clsx(styles.button, styles.getStarted)}>
+        Explore features →
+      </a>
+    </div>
   </section>
 );
