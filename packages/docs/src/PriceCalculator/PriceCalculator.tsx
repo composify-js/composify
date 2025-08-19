eee30120e8192dc029dfba9631182f195c165a3a
Injung Chung <injung.chung@gmail.com>
2025-08-19 20:47:14 +0900

    fix: dynamically change range value

--- packages/docs/src/PriceCalculator/PriceCalculator.tsx
+++ packages/docs/src/PriceCalculator/PriceCalculator.tsx
@@ -1,15 +1,15 @@
 import { useState, type FC } from 'react';
 import styles from './PriceCalculator.module.css';
 
-const PAGES_MAX = 1000;
-const SEATS_MAX = 100;
+const PAGES = ['1', '5', '10', '20', '30', '50', '100', '250', '500', '1000', '2500', '5000+'];
+const SEATS = ['1', '2', '3', '5', '10', '15', '20', '30', '40', '50', '75', '100+'];
 
 export const PriceCalculator: FC = () => {
   const [pages, setPages] = useState(1);
   const [seats, setSeats] = useState(1);
 
-  const pagesProgress = (pages / PAGES_MAX) * 100;
-  const seatsProgress = (seats / SEATS_MAX) * 100;
+  const pagesProgress = (pages / (PAGES.length - 1)) * 100;
+  const seatsProgress = (seats / (SEATS.length - 1)) * 100;
 
   const price = Math.max((pages - 1) * 1 + (seats - 1) * 5, 0);
 
@@ -20,51 +20,49 @@ export const PriceCalculator: FC = () => {
           <div className={styles.header}>
             <p className={styles.title}>Live Pages</p>
             <p className={styles.usage}>
-              {pages.toLocaleString()} page{pages !== 1 ? 's' : ''}
+              {PAGES[pages]} page{pages > 0 ? 's' : ''}
             </p>
           </div>
           <input
             type="range"
             step={1}
-            min={1}
-            max={PAGES_MAX}
+            min={0}
+            max={PAGES.length - 1}
             value={pages}
             className={styles.rangeInput}
             onChange={e => setPages(Number(e.target.value))}
             style={{ '--progress': `${pagesProgress}%` } as React.CSSProperties}
           />
           <div className={styles.indicatorGroup}>
-            <p className={styles.indicator}>0</p>
-            <p className={styles.indicator}>250</p>
-            <p className={styles.indicator}>500</p>
-            <p className={styles.indicator}>750</p>
-            <p className={styles.indicator}>1000+</p>
+            <p className={styles.indicator}>{PAGES[0]}</p>
+            <p className={styles.indicator}>{PAGES[3]}</p>
+            <p className={styles.indicator}>{PAGES[7]}</p>
+            <p className={styles.indicator}>{PAGES[11]}</p>
           </div>
         </div>
         <div className={styles.divider} />
         <div className={styles.calculator}>
           <div className={styles.header}>
-            <p className={styles.title}>Editors</p>
+            <p className={styles.title}>Editor seats</p>
             <p className={styles.usage}>
-              {seats.toLocaleString()} seat{seats !== 1 ? 's' : ''}
+              {SEATS[seats]} seat{seats > 0 ? 's' : ''}
             </p>
           </div>
           <input
             type="range"
             step={1}
-            min={1}
-            max={SEATS_MAX}
+            min={0}
+            max={SEATS.length - 1}
             value={seats}
             className={styles.rangeInput}
             onChange={e => setSeats(Number(e.target.value))}
             style={{ '--progress': `${seatsProgress}%` } as React.CSSProperties}
           />
           <div className={styles.indicatorGroup}>
-            <p className={styles.indicator}>0</p>
-            <p className={styles.indicator}>25</p>
-            <p className={styles.indicator}>50</p>
-            <p className={styles.indicator}>75</p>
-            <p className={styles.indicator}>100+</p>
+            <p className={styles.indicator}>{SEATS[0]}</p>
+            <p className={styles.indicator}>{SEATS[3]}</p>
+            <p className={styles.indicator}>{SEATS[7]}</p>
+            <p className={styles.indicator}>{SEATS[11]}</p>
           </div>
         </div>
       </div>
