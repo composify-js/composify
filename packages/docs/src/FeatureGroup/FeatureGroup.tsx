2a9828f5383add0433a231eafb6782a504b9ec0c
Injung Chung <injung.chung@gmail.com>
2025-08-20 23:31:08 +0900

    feat: add id to the paid features

--- packages/docs/src/FeatureGroup/FeatureGroup.tsx
+++ packages/docs/src/FeatureGroup/FeatureGroup.tsx
@@ -2,12 +2,12 @@ import { type FC, type PropsWithChildren } from 'react';
 import styles from './FeatureGroup.module.css';
 
 type Props = PropsWithChildren<{
+  id?: string;
   title: string;
   description: string;
 }>;
-
-export const FeatureGroup: FC<Props> = ({ title, description, children }) => (
-  <section className={styles.container}>
+export const FeatureGroup: FC<Props> = ({ id, title, description, children }) => (
+  <section id={id} className={styles.container}>
     <h2 className={styles.title}>{title}</h2>
     <p className={styles.description}>{nl2br(description)}</p>
     <div className={styles.features}>{children}</div>
