7cf783a53fb19ac95e7315bfdc9230e2b3effdc4
Injung Chung <injung.chung@gmail.com>
2025-09-13 09:37:43 +0900

    feat: add utils to entry points

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
 export * from './editor';
 export * from './renderer';
+export * from './utils';
--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   format: ['cjs', 'esm'],
-  entry: ['src/index.ts', 'src/editor/index.ts', 'src/renderer/index.ts'],
+  entry: ['src/index.ts', 'src/editor/index.ts', 'src/renderer/index.ts', 'src/utils/index.ts'],
   outDir: 'dist',
   sourcemap: true,
   dts: true,
