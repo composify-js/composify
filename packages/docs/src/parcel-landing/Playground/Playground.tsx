80f9922015852519ece0f00a5c62e86053a88e6a
Injung Chung <injung.chung@gmail.com>
2025-12-17 13:44:05 +0900

    fix: do not scale playground on desktop viewports

--- packages/docs/src/parcel-landing/Playground/Playground.tsx
+++ packages/docs/src/parcel-landing/Playground/Playground.tsx
@@ -3,7 +3,7 @@ import { Editor, useComposifyEditor } from '@composify/react/editor';
 import clsx from 'clsx';
 import { getBox } from 'css-box-model';
 import { type FC, useEffect, useRef, useState } from 'react';
-import { useSource } from '../SourceContext';
+import { SourceEditor, useSource } from '../SourceContext';
 import styles from './Playground.module.css';
 
 const PLAYGROUND_WIDTH = 1293;
@@ -58,45 +58,50 @@ export const Playground: FC<Props> = ({ plain }) => {
   return (
     <section className={clsx(styles.container, plain && styles.plain)}>
       <div ref={previewRef} className={styles.preview} style={{ aspectRatio }}>
-        <div
-          className={styles.viewport}
-          style={{
-            width: PLAYGROUND_WIDTH,
-            aspectRatio,
-            transform: `scale(${scale})`,
-          }}
-        >
-          {isLibrary || isVisualEditor ? (
-            <div className={styles.mock}>
-              <h2 className={styles.mockTitle}>🎨 Playground</h2>
+        {isLibrary || isVisualEditor ? (
+          <div className={styles.mock}>
+            <h2 className={styles.mockTitle}>🎨 Playground</h2>
+          </div>
+        ) : (
+          <>
+            <div className={styles.desktop}>
+              <SourceEditor />
             </div>
-          ) : (
-            <Editor
-              title="Home Page"
-              source={source}
-              viewports={[
-                {
-                  width: 425,
-                  label: 'Mobile - 425px',
-                },
-                {
-                  width: 768,
-                  label: 'Tablet - 768px',
-                  initial: true,
-                },
-                {
-                  width: 1024,
-                  label: 'Laptop - 1024px',
-                },
-                {
-                  width: 1440,
-                  label: 'Desktop - 1440px',
-                },
-              ]}
-              onSubmit={setSource}
-            />
-          )}
-        </div>
+            <div
+              className={styles.mobile}
+              style={{
+                width: PLAYGROUND_WIDTH,
+                aspectRatio,
+                transform: `scale(${scale})`,
+              }}
+            >
+              <Editor
+                title="Home Page"
+                source={source}
+                viewports={[
+                  {
+                    width: 425,
+                    label: 'Mobile - 425px',
+                  },
+                  {
+                    width: 768,
+                    label: 'Tablet - 768px',
+                    initial: true,
+                  },
+                  {
+                    width: 1024,
+                    label: 'Laptop - 1024px',
+                  },
+                  {
+                    width: 1440,
+                    label: 'Desktop - 1440px',
+                  },
+                ]}
+                onSubmit={setSource}
+              />
+            </div>
+          </>
+        )}
       </div>
       {!plain && <p className={styles.description}>Make a change, hit save, and... tada! 🎉</p>}
     </section>
